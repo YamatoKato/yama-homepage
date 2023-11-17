75f4e8ee45605466b6759f7d471e0af243a38849
YamatoKato <yamatotdu@ezweb.ne.jp>
2023-11-18 05:28:08 +0900

    fix: warningの対応

--- components/Blog/Zenn.tsx
+++ components/Blog/Zenn.tsx
@@ -3,7 +3,6 @@ import Link from 'next/link';
 import { ZennArticleType, ZennProps } from '../../types/types';
 import dayjs from 'dayjs';
 const Zenn: React.FC<ZennProps> = ({ posts }) => {
-  // 横クロールで表示したい
   return (
     <div>
       <h1 className='text-2xl font-semibold px-2 pt-4'>Zenn記事↓ </h1>
@@ -19,8 +18,8 @@ const Zenn: React.FC<ZennProps> = ({ posts }) => {
           <ul className='flex space-x-4 p-4'>
             {posts.map((post: ZennArticleType) => (
               <li key={post.id} className='flex-none justify-center'>
-                <Link href={`https://zenn.dev/${post.path}`} target='blank'>
-                  <div className=' px-3 border rounded-md shadow-md hover:shadow-lg transition duration-300 transform hover:scale-105 bg-sky-100 w-96'>
+                <div className=' px-3 border rounded-md shadow-md hover:shadow-lg transition duration-300 transform hover:scale-105 bg-sky-100 w-96'>
+                  <Link href={`https://zenn.dev${post.path}`} target='blank'>
                     <p className='text-lg font-semibold my-2 break-words'>
                       {post.emoji}
                       {post.title}
@@ -28,8 +27,8 @@ const Zenn: React.FC<ZennProps> = ({ posts }) => {
                     <p className='text-sm text-gray-500'>
                       {dayjs(post.published_at).format('YYYY/MM/DD')}
                     </p>
-                  </div>
-                </Link>
+                  </Link>
+                </div>
               </li>
             ))}
           </ul>
